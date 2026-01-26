a848e16985ddf350c7761ac2aa9a9f7a71e89178
kaspi <nikita.fedyushin@kaspi.kz>
2026-01-26 18:45:28 +0500

    adapt single spa for system js child project format

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,22 @@ import { resolve } from 'path';
 export default defineConfig({
   root: './src',
   build: {
-    outDir: '../dist',
+    outDir: "dist",
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
+      input: "src/root-config.ts",
+      external: ["single-spa"],
+      preserveEntrySignatures: "allow-extension",
+      output: {
+        format: "system",
+        entryFileNames: "root-config.js",
+      },
     },
   },
   server: {
     port: 9000,
     cors: true
   },
-  publicDir: false,
 });
 
